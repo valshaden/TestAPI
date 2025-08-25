30222bf9a8b49e77af911ba31b1ff038a334ec2a
valshaden <valshaden@yandex.ru>
2025-08-25 12:26:38 +0300

    + readme

---
+++ 01.py
@@ -0,0 +1,19 @@
+# test Fast API
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+
