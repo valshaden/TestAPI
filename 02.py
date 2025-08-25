07b6e06c47496a41bc28273018abe162b0022d1d
valshaden <valshaden@yandex.ru>
2025-08-25 12:45:11 +0300

    ++

--- 02.py
+++ 02.py
@@ -4,15 +4,15 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/")
+#async def read_root():
 def read_root():
     return {"message": "Hello World-2"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"ты ввел = ": item_id, "q": q}
 
 
-
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
--- app_01.py
+++ app_01.py
@@ -4,15 +4,9 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hello World-2"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"ты ввел = ": item_id, "q": q}
-
-
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
